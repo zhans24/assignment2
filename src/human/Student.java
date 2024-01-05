c3dbfdc1440f3732a6f354f4ff3850feee56015f
zhanserik <zhasikgaziz@gmail.com>
2024-01-06 00:01:38 +0600

    aitu

---
+++ src/human/Student.java
@@ -0,0 +1,36 @@
+package human;
+
+import Interface.Payable;
+
+public class Student extends Person implements Payable {
+    private double gpa;
+
+    public Student(){
+        super();
+        setGpa(gpa);
+    }
+
+    public Student(String name,String surname,double gpa){
+        super(name,surname);
+        this.gpa=gpa;
+
+    }
+    public void setGpa(double gpa) {
+        this.gpa = gpa;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+
+    @Override
+    public double getPaymentAmount() {
+        if (getGpa()>2.67) return 36660;
+        else return 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Student:"+super.toString();
+    }
+}
