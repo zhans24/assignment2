2cce7838c7f70ee315e1cfae7d1d5e50d5841326
zhanserik <zhasikgaziz@gmail.com>
2024-01-06 00:11:05 +0600

    aitu

--- src/Main.java
+++ src/Main.java
@@ -9,9 +9,17 @@ class Main{
         Person s=new Student("Zhans","Gaziz",2.87);
         Person e=new Employee("Erlan","Kenesbay","Busy",400000);
         Person a=new Student("Nurs","Oryn",2.5);
-        List<Person> list=Arrays.asList(s,e,a);
+
+        Student b=new Student();
+        Employee c=new Employee();
+
+        b.setName("Almas");b.setSurname("Zhas");b.setGpa(2.90);
+        c.setName("Kamal");c.setSurname("Baba");c.setSalary(600000);
+
+        List<Person> list=Arrays.asList(s,e,a,b,c);
         Collections.sort(list);
         printData(list);
+
     }
 
     public static void printData(Iterable<Person> people){
